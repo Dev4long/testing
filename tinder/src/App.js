543253fb9126d170975cdc832c9b2bea7de6faf8
denis kadrich <dev4long@live.com>
2022-04-05 19:45:58 -0400

    up and going

--- tinder/src/App.js
+++ tinder/src/App.js
@@ -1,9 +1,15 @@
 import './App.css';
+import Sidebar from "./Sidebar";
+import Chat from "./Chat"
 
 function App() {
   return (
-    <div className="App">
-     <div>hello</div>
+    <div className="app">
+       <div className= "app_body">
+        hello
+        <Sidebar />
+        <Chat />
+       </div>
     </div>
   );
 }
---
+++ tinder/src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAYP-bl5ofO00KTzKg0Mkk2rqXE-MnJRIE",
+    authDomain: "whatsapp-clone-ff1c0.firebaseapp.com",
+    projectId: "whatsapp-clone-ff1c0",
+    storageBucket: "whatsapp-clone-ff1c0.appspot.com",
+    messagingSenderId: "473637349010",
+    appId: "1:473637349010:web:fb04b8625f99dfc323684b"
+  };
