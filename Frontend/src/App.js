1532bf38ca9a6214410d6e623ab74c30bbac6b11
denis kadrich <dev4long@live.com>
2022-05-02 10:29:02 -0400

    ok done

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,13 +1,46 @@
 import './App.css';
 import Sidebar from "./Sidebar";
 import Chat from "./Chat"
+import React, {useEffect, useState} from "react"
+import Pusher from 'pusher-js'
+import axios from "./axios"
 
 function App() {
+
+  const [messages, setMessages] = useState([])
+
+  useEffect(() => {
+    axios.get('/messages/sync')
+    .then(response => {
+      setMessages(response.data);
+
+    })
+  }, [])
+
+  console.log(messages)
+
+  useEffect(() => {
+    const pusher = new Pusher('c008b700a45c59b2120f', {
+      cluster: 'us2'
+    });
+    const channel = pusher.subscribe('messages');
+    channel.bind('inserted', (newMSG) => {
+      setMessages([...messages, newMSG])
+    });
+
+   return () => {
+     channel.unbind_all();
+     channel.unsubscribe()
+   }
+
+  }, [messages])
+
+
   return (
     <div className="app">
        <div className= "app_body">
         <Sidebar />
-        <Chat />
+        <Chat messages={messages} />
        </div>
     </div>
   );
--- Frontend/src/Chat.js
+++ Frontend/src/Chat.js
@@ -1,9 +1,27 @@
-import React from 'react'
+import React, {useState}from 'react'
 import "./Chat.css"
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
 import {faPaperclip, faGripVertical, faUser, faMagnifyingGlass, faFaceGrin, faMicrophone} from "@fortawesome/free-solid-svg-icons";
+import axios from "./axios"
 
-function Chat() {
+function Chat({messages}) {
+  
+  const [input, setInput] = useState("");
+
+  const sendMessage = async (e) => {
+    e.preventDefault();
+
+    await axios.post('/messages/new', {
+    "message": input,
+    "name": "d5",
+    "timestamp": "just now",
+    "received": false
+    });
+
+    setInput("")
+  }
+
+
   return (
     <div className ="chat">
       <div className="chat_header">
@@ -21,28 +39,20 @@ function Chat() {
         </div>
 
         <div className="chat_body">
-          <p className= "chat_message">
-            <span className="chat_name">denis</span>
-            Messages
-            <span className ="chat_timestamp"> {new Date().toUTCString()}</span>
+          {messages.map((message) => (
+            <p className= {`chat_message ${message.received && "chat_receiver"}`}>
+            <span className="chat_name">{message.name}</span>
+            {message.message}
+            <span className ="chat_timestamp"> {message.timestamp}</span>
           </p>
-          <p className= "chat_message chat_reciever">
-            <span className="chat_name">denis</span>
-            Messages
-            <span className ="chat_timestamp"> {new Date().toUTCString()}</span>
-          </p>
-          <p className= "chat_message">
-            <span className="chat_name">denis</span>
-            Messages
-            <span className ="chat_timestamp"> {new Date().toUTCString()}</span>
-          </p>
+          ))}
         </div>
 
         <div className= "chat_footer">
            <FontAwesomeIcon className= "chatIcons" icon={faFaceGrin} />
            <form>
-             <input  placeholder="Type a message" type="text" />
-             <button type="submit">Send Message</button>
+             <input value={input} onChange={e => setInput(e.target.value)} placeholder="Type a message" type="text" />
+             <button onClick={sendMessage} type="submit">Send Message</button>
            </form>
            <FontAwesomeIcon className= "chatIcons" icon={faMicrophone} />
         </div>
